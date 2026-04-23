1ea607ae7b27bf962b34a748c47c9695cce7f5c8
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-04-23 19:14:30 +0900

    実行速度確認リスト追加 #2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Parallel_Practice;
+﻿using System.Diagnostics;
+
+namespace Parallel_Practice;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // 実行速度確認リスト
+        var actions = new List<Action>
+        {
+           () =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"Hello, World11! {DateTime.Now.ToString("HH:mm:ss.fff")}");
+            },
+           () =>
+                {
+                Thread.Sleep(1000);
+                Console.WriteLine($"Hello, World2! {DateTime.Now.ToString("HH:mm:ss.fff")}");
+            },
+            () =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"Hello, World13 {DateTime.Now.ToString("HH:mm:ss.fff")}");
+            }
+        };
     }
 }
